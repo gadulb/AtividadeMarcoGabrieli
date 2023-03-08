77a1f346214cacefa467d31519c3243ef5d511d3
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2023-03-08 16:39:30 -0300

    add

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 function HomeScreen() {
   return (
@@ -13,7 +14,7 @@ function HomeScreen() {
 
 function AboutScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Esta é uma atividade realizada por Gabrieli nas aulas de aplicações web.</Text>
     </View>
   );
@@ -33,9 +34,30 @@ export default function App(){
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="About" component={AboutScreen} />
-        <Tab.Screen name="Contato" component={ContatoScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} 
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen name="About" component={AboutScreen} 
+        /*options={{
+          tabBarLabel: 'About',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="about" color={color} size={26} />
+            ),
+          }}*/
+        />
+        <Tab.Screen name="Contato" component={ContatoScreen} 
+        /*options={{
+          tabBarLabel: 'Contato',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="contact" color={color} size={26} />
+            ),
+          }}*/
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
